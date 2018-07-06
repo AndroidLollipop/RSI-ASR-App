9d270191b1daef98115d533b3dc735ec46225c68
AndroidLollipop <obfuscated00143241131643657478@gmail.com>
2018-07-06 21:46:42 +0800

    Hopefully this will work after caf to ogg conversion

--- fetchData.js
+++ fetchData.js
@@ -1,83 +1,9 @@
 //fake server, wire this up to the server later
 var getAsrText = async (uri) => {
-  const ajax = function (option) { // $.ajax(...) without jquery.
-    if (typeof(option.url) == "undefined") {
-        try {
-            option.url = location.href;
-        } catch(e) {
-            var ajaxLocation;
-            ajaxLocation = document.createElement("a");
-            ajaxLocation.href = "";
-            option.url = ajaxLocation.href;
-        }
-    }
-    if (typeof(option.type) == "undefined") {
-        option.type = "GET";
-    }
-    if (typeof(option.data) == "undefined") {
-        option.data = null;
-    } else {
-        console.log(option.data.length)
-        /*var data = "";
-        for (var x in option.data) {
-            if (data != "") {
-                data += "&";
-            }
-            data += encodeURIComponent(x)+"="+encodeURIComponent(option.data[x]);
-        };
-        option.data = data;*/
-    }
-    if (typeof(option.statusCode) == "undefined") { // 4
-        option.statusCode = {};
-    }
-    if (typeof(option.beforeSend) == "undefined") { // 1
-        option.beforeSend = function () {};
-    }
-    if (typeof(option.success) == "undefined") { // 4 et sans erreur
-        option.success = function () {};
-    }
-    if (typeof(option.error) == "undefined") { // 4 et avec erreur
-        option.error = function () {};
-    }
-    if (typeof(option.complete) == "undefined") { // 4
-        option.complete = function () {};
-    }
-    typeof(option.statusCode["404"]);
-
-    var xhr = null;
-
-    if (window.XMLHttpRequest || window.ActiveXObject) {
-        if (window.ActiveXObject) { try { xhr = new ActiveXObject("Msxml2.XMLHTTP"); } catch(e) { xhr = new ActiveXObject("Microsoft.XMLHTTP"); } }
-        else { xhr = new XMLHttpRequest(); }
-    } else { alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest..."); return null; }
-
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState == 1) {
-            option.beforeSend();
-        }
-        if (xhr.readyState == 4) {
-            option.complete(xhr, xhr.status);
-            if (xhr.status == 200 || xhr.status == 0) {
-                option.success(xhr.responseText);
-            } else {
-                option.error(xhr.status);
-                if (typeof(option.statusCode[xhr.status]) != "undefined") {
-                    option.statusCode[xhr.status]();
-                }
-            }
-        }
-    };
-
-    if (option.type == "POST") {
-        xhr.open(option.type, option.url, true);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-        xhr.send(option.data);
-    } else {
-        xhr.open(option.type, option.url+option.data, true);
-        xhr.send(null);
-    }
-
-}
+  var resolveMyPromise
+  const myPromise = new Promise(resolve => {
+    resolveMyPromise = resolve
+  })
   const rippedfromimda = function() {
     console.log("hello")
     console.log("sendAsr: chunks length: %d", blob.size);
@@ -112,9 +38,31 @@ var getAsrText = async (uri) => {
   }
   const res = await fetch(uri)
   const blob = await res.blob()
-  console.log(blob)
-  rippedfromimda()
-  //awwwwwww yisssss
+  const url = "http://192.168.1.31/speech/english/imda1.php"
+  var myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json; charset=utf-8');
+  var reader = new FileReader()
+  reader.readAsDataURL(blob)
+  reader.onloadend = async function (){
+    var wavContent = reader.result
+    wavContent = wavContent.replace("data:application/octet-stream;base64,", "")
+    var myObj = { "Wavfile": "FromBrowser.ogg",
+      "EncodedSpeech": wavContent
+    }
+    var myInit = { method: "POST",
+      headers: myHeaders,
+      body: JSON.stringify(wavContent)
+    }
+    console.log("before")
+    try {
+      var res = await fetch(url, myInit)
+    }
+    catch(e){
+      alert(e)
+    }
+    console.log("after")
+    console.log(res)
+  }
   return new Promise(resolve => {
     setTimeout(() => {
       resolve("where is the bubble gum")
